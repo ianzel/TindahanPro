81f1a088c8b1e221d7a9a38eb6ff61503e6b7e7c
ianzel <imorales@ssct.edu.ph>
2026-04-27 23:17:15 +0800

    Updated UI

--- backend/src/suppliers/suppliers.controller.ts
+++ backend/src/suppliers/suppliers.controller.ts
@@ -3,41 +3,20 @@ import { SuppliersService } from './suppliers.service';
 
 @Controller('suppliers')
 export class SuppliersController {
-  constructor(private readonly service: SuppliersService) {}
+  constructor(private readonly suppliersService: SuppliersService) {}
 
   @Get()
   findAll() {
-    return this.service.findAll();
+    return this.suppliersService.findAll();
   }
 
   @Post()
   create(@Body() data: any) {
-    return this.service.create(data);
+    return this.suppliersService.create(data);
   }
 
   @Delete(':id')
   remove(@Param('id') id: number) {
-    return this.service.remove(id);
-  }
-}import { Controller, Get, Post, Delete, Param, Body } from '@nestjs/common';
-import { SuppliersService } from './suppliers.service';
-
-@Controller('suppliers')
-export class SuppliersController {
-  constructor(private readonly service: SuppliersService) {}
-
-  @Get()
-  findAll() {
-    return this.service.findAll();
-  }
-
-  @Post()
-  create(@Body() data: any) {
-    return this.service.create(data);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: number) {
-    return this.service.remove(id);
+    return this.suppliersService.remove(Number(id));
   }
 }
